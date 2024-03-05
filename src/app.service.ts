380437771bf3c1717a1b36a302cf54c74d6a4774
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-03-05 19:36:47 +0500

    make root route public

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Public } from 'src/decorators/skip-auth';
 
 @Injectable()
 export class AppService {
+  @Public()
   get(): string {
     return 'Nothing to do here';
   }
